116add293aaafdf106b790a99480ec4e3bf7c408
tonimjdev <tonimdev@gmail.com>
2022-10-03 10:28:00 +0200

    Exercici 1 - Component Home - Checkbox i càlcul total pressupost

--- src/app/pressupost/home/home.component.ts
+++ src/app/pressupost/home/home.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { variationPlacements } from '@popperjs/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+  web: number = 500;
+  seo: number = 300;
+  pub: number = 200;
 
-  ngOnInit(): void {
+  totalServicios: number = 0;
+  acumulado: number = 0;
+
+  calcularTotal(valor:any):void {
+    if (valor.currentTarget.checked) {
+      if (valor.target.defaultValue === "web") this.acumulado = this.web;
+      else if (valor.target.defaultValue === "seo") this.acumulado = this.seo;
+      else this.acumulado = this.pub;
+      this.totalServicios+=this.acumulado;
+    } else {
+      if (valor.target.defaultValue === "web") this.acumulado = this.web;
+      else if (valor.target.defaultValue === "seo") this.acumulado = this.seo;
+      else this.acumulado = this.pub;
+      this.totalServicios-=this.acumulado;
+    }
   }
-
 }
