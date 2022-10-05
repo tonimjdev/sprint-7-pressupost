8170b6dd7e93987750d23b646121eccece928f09
tonimjdev <tonimdev@gmail.com>
2022-10-05 11:40:47 +0200

    Exercici 3 - Botons & Funcions per càlcul a temps real

--- src/app/pressupost/panell/panell.component.ts
+++ src/app/pressupost/panell/panell.component.ts
@@ -11,33 +11,41 @@ import { PressupostService } from '../services/pressupost.service';
 export class PanellComponent {
   // Formulari reactiu validators (minim 1 pag 1 idioma)
   panellFormulari: FormGroup = this.fb.group({
-    pagControl: [0, [Validators.required, Validators.min(1)]],
-    idiomaControl: [0, [Validators.required, Validators.min(1)]],
+    pagControl: [1, [Validators.required, Validators.min(1)]],
+    idiomaControl: [1, [Validators.required, Validators.min(1)]],
   });
+
   // Missatge error a l'HTML si no passa validació
   campEsValid(campo: string) {
     return this.panellFormulari.controls[campo].errors;
   }
-  pagines: number = 1;
-  idiomes: number = 1;
+
+  // Variables
+  pagines: number = 0;
+  idiomes: number = 0;
   totalOpcionsWeb: number = 0;
 
-  // Injectem el servei pressupost
+  // Injectem el servei pressupost al constructor
   constructor(
     public pressupostService: PressupostService,
     private fb: FormBuilder
   ) {}
 
-  // Mètode que retorna el Total Opcions Web utilitzant el servei
-  totalWebOpcions() {
-    this.pagines = Number(this.panellFormulari.get('pagControl')?.value);
-    this.idiomes = Number(this.panellFormulari.get('idiomaControl')?.value);
+  cridarServeiOpcionsWeb() {
     if (this.pagines < 1 || this.idiomes < 1) this.totalOpcionsWeb = 0;
-    else
+    else {
       this.totalOpcionsWeb = this.pressupostService.calcOpcionsWeb(
         this.pagines,
         this.idiomes
       );
+    }
+  }
+
+  // Mètode que retorna el Total Opcions Web utilitzant el servei
+  totalWebOpcions() {
+    this.pagines = Number(this.panellFormulari.get('pagControl')?.value);
+    this.idiomes = Number(this.panellFormulari.get('idiomaControl')?.value);
+    this.cridarServeiOpcionsWeb();
   }
 
   // Enviem Total Opcions Web al component pare "Home" amb @Output
@@ -45,5 +53,19 @@ export class PanellComponent {
   propagar = new EventEmitter<number>();
   onPropagar() {
     this.propagar.emit(this.totalOpcionsWeb);
+  }
+
+  // Operar amb botons
+  sumar(servei: string) {
+    servei === 'pagines' ? this.pagines++ : this.idiomes++;
+
+    this.cridarServeiOpcionsWeb();
+    this.onPropagar();
+  }
+  restar(servei: string) {
+    servei === 'pagines' ? this.pagines-- : this.idiomes--;
+
+    this.cridarServeiOpcionsWeb();
+    this.onPropagar();
   }
 }
